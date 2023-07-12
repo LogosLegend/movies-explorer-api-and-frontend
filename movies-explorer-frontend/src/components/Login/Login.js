5e0ffd4c8fba02b52c2c7da11b5e660cd6be3119
LogosLegend <75035186+LogosLegend@users.noreply.github.com>
2023-07-12 09:30:36 +0600

    Добавление блокировки полей формы "Login" во время отправки запросов

--- movies-explorer-frontend/src/components/Login/Login.js
+++ movies-explorer-frontend/src/components/Login/Login.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { useInput } from '../../utils/FormValidation.js';
 
 function Login(props) {
+
+  const [fieldDisabled, setFieldDisabled] = useState(false);
 
   const email = useInput('', {empty: true, email: true})
   const password = useInput('', {empty: true})
@@ -11,7 +14,8 @@ function Login(props) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.onLogin(email.value, password.value)
+    setFieldDisabled(true)
+    props.onLogin(email.value, password.value, setFieldDisabled)
   }
 
   return(
@@ -21,19 +25,19 @@ function Login(props) {
         <form onSubmit={handleSubmit} className="auth__form">
 
           <div className="auth__input-container">
-            <input className={`auth__input ${(email.email && email.isDirty) ? `auth__input_type_error` : ``}`} required id="email" name="email" type="email" placeholder="E-mail" value={email.value} onChange={email.handleChange} onBlur={email.handleDirty}/>
+            <input className={`auth__input ${(email.email && email.isDirty) ? `auth__input_type_error` : ``}`} required id="email" name="email" type="email" placeholder="E-mail" value={email.value} onChange={email.handleChange} onBlur={email.handleDirty} disabled={fieldDisabled ? true : ``}/>
             <label className="auth__label" htmlFor="email" title="E-mail"></label>
             <span className={`error auth__span-error ${(email.email && email.isDirty) ? `visible` : ``}`}>{email.empty ? `Введите адрес электронной почты` : `Неверный адрес электронной почты`}</span>
           </div>
 
           <div className="auth__input-container">
-            <input className={`auth__input ${(password.empty && password.isDirty) ? `auth__input_type_error` : ``}`} required id="password" name="password" type="password" placeholder="Пароль" value={password.value} onChange={password.handleChange} onBlur={password.handleDirty}/>
+            <input className={`auth__input ${(password.empty && password.isDirty) ? `auth__input_type_error` : ``}`} required id="password" name="password" type="password" placeholder="Пароль" value={password.value} onChange={password.handleChange} onBlur={password.handleDirty} disabled={fieldDisabled ? true : ``}/>
             <label className="auth__label" htmlFor="password" title="Пароль"></label>
             <span className={`error auth__span-error ${(password.empty && password.isDirty) ? `visible` : ``}`}>Введите пароль</span>
           </div>
 
           <span className="error auth__error">{props.message}</span>
-          <button type="submit" className={`auth__button button-hovered ${handleButtonDisabled() ? `` : `auth__button_disable`}`} disabled={handleButtonDisabled() ? `` : true}>Войти</button>
+          <button type="submit" className={`auth__button button-hovered ${!handleButtonDisabled() || fieldDisabled ? `auth__button_disable` : ``}`} disabled={!handleButtonDisabled() || fieldDisabled ? true : ``}>Войти</button>
         </form>
 
         <p className="auth__switch">Ещё не зарегистрированы? <button className="auth__switch-link button-without-styles button-hovered" onClick={props.onClickNavigateSignUp}>Регистрация</button>
