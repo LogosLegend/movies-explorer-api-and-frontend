e1439115e154de6b4062ca756a9cad2afd52240c
LogosLegend <75035186+LogosLegend@users.noreply.github.com>
2023-07-12 08:35:04 +0600

    Добавление блокировки полей формы "Profile" во время отправки запросов

--- movies-explorer-frontend/src/components/Profile/Profile.js
+++ movies-explorer-frontend/src/components/Profile/Profile.js
@@ -5,6 +5,7 @@ import { useInput } from '../../utils/FormValidation.js';
 function Profile(props) {
 
   const currentUser = useContext(CurrentUserContext);
+  const [fieldDisabled, setFieldDisabled] = useState(false);
   const [buttonDisabled, setButtonDisabled] = useState(true);
 
   const name = useInput(currentUser.name, {minMaxLength: {min: 2, max: 30}});
@@ -15,7 +16,7 @@ function Profile(props) {
     if ((name.value !== currentUser.name || email.value !== currentUser.email) && name.inputValid && email.inputValid) {
 
       setButtonDisabled(false)
-      
+
     } else {
 
       setButtonDisabled(true)
@@ -24,7 +25,8 @@ function Profile(props) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.onProfile(name.value, email.value)
+    setFieldDisabled(true)
+    props.onProfile(name.value, email.value, setFieldDisabled)
   }
 
   return (
@@ -35,18 +37,18 @@ function Profile(props) {
         <form onSubmit={handleSubmit} className="account__form">
           <div className="account__info">
             <p className="account__name">Имя</p>
-            <input className={`account__input ${(name.minMaxLength && name.isDirty) ? `account__input_type_error` : ``}`} required id="name" name="name" type="text" placeholder="Имя" minLength="2" maxLength="30" value={name.value} onChange={name.handleChange} onBlur={name.handleDirty}/>
+            <input className={`account__input ${(name.minMaxLength && name.isDirty) ? `account__input_type_error` : ``}`} required id="name" name="name" type="text" placeholder="Имя" minLength="2" maxLength="30" value={name.value} onChange={name.handleChange} onBlur={name.handleDirty} disabled={fieldDisabled ? true : ``}/>
             <span className={`error account__span-error ${(name.minMaxLength && name.isDirty) ? `visible` : ``}`}>Имя должно содержать от 2 до 30 символов</span>
           </div>
 
           <div className="account__info">
             <p className="account__name">E-mail</p>
-            <input className={`account__input ${(email.email && email.isDirty) ? `account__input_type_error` : ``}`} required id="email" name="email" type="email" placeholder="E-mail" value={email.value} onChange={email.handleChange} onBlur={email.handleDirty}/>
+            <input className={`account__input ${(email.email && email.isDirty) ? `account__input_type_error` : ``}`} required id="email" name="email" type="email" placeholder="E-mail" value={email.value} onChange={email.handleChange} onBlur={email.handleDirty} disabled={fieldDisabled ? true : ``}/>
             <span className={`error account__span-error ${(email.email && email.isDirty) ? `visible` : ``}`}>{email.empty ? `Введите адрес электронной почты` : `Неверный адрес электронной почты`}</span>
           </div>
 
           <span className={`error ${props.success ? `account__success` : `account__error`}`}>{props.message}</span>
-          <button className={`account__button account__submit-button button-without-styles button-hovered ${buttonDisabled ? `account__submit-button_disable` : ``}`} disabled={buttonDisabled ? true : ``}>Редактировать</button>
+          <button className={`account__button account__submit-button button-without-styles button-hovered ${buttonDisabled || fieldDisabled ? `account__submit-button_disable` : ``}`} disabled={buttonDisabled || fieldDisabled ? true : ``}>Редактировать</button>
         </form>
 
         <button className="account__button account__exit-button button-without-styles button-hovered" onClick={props.onExit}>Выйти из аккаунта</button>
