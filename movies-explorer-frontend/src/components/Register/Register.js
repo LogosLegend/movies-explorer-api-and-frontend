6959e42122a5736ff2feeb3da0c1d9f63931d003
LogosLegend <75035186+LogosLegend@users.noreply.github.com>
2023-07-12 10:56:06 +0600

    Добавление констант Register

--- movies-explorer-frontend/src/components/Register/Register.js
+++ movies-explorer-frontend/src/components/Register/Register.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useInput } from '../../utils/FormValidation.js';
+import { NAME_WRONG_LENGTH, EMAIL_NOT_FOUND, EMAIL_INCORRECT, PASSWORD_NOT_FOUND } from '../../utils/constants.js';
 
 function Register(props) {
 
@@ -28,19 +29,19 @@ function Register(props) {
           <div className="auth__input-container">
             <input className={`auth__input ${(name.minMaxLength && name.isDirty) ? `auth__input_type_error` : ``}`} required id="name" name="name" type="text" placeholder="Имя" minLength="2" maxLength="30" value={name.value} onChange={name.handleChange} onBlur={name.handleDirty} disabled={fieldDisabled ? true : ``}/>
             <label className="auth__label" htmlFor="name" title="Имя"></label>
-            <span className={`error auth__span-error ${(name.minMaxLength && name.isDirty) ? `visible` : ``}`}>Имя должно содержать от 2 до 30 символов</span>
+            <span className={`error auth__span-error ${(name.minMaxLength && name.isDirty) ? `visible` : ``}`}>{NAME_WRONG_LENGTH}</span>
           </div>
 
           <div className="auth__input-container">
             <input className={`auth__input ${(email.email && email.isDirty) ? `auth__input_type_error` : ``}`} required id="email" name="email" type="email" placeholder="E-mail" value={email.value} onChange={email.handleChange} onBlur={email.handleDirty} disabled={fieldDisabled ? true : ``}/>
             <label className="auth__label" htmlFor="email" title="E-mail"></label>
-            <span className={`error auth__span-error ${(email.email && email.isDirty) ? `visible` : ``}`}>{email.empty ? `Введите адрес электронной почты` : `Неверный адрес электронной почты`}</span>
+            <span className={`error auth__span-error ${(email.email && email.isDirty) ? `visible` : ``}`}>{email.empty ? EMAIL_NOT_FOUND : EMAIL_INCORRECT}</span>
           </div>
 
           <div className="auth__input-container">
             <input className={`auth__input ${(password.empty && password.isDirty) ? `auth__input_type_error` : ``}`} required id="password" name="password" type="password" placeholder="Пароль" value={password.value} onChange={password.handleChange} onBlur={password.handleDirty} disabled={fieldDisabled ? true : ``}/>
             <label className="auth__label" htmlFor="password" title="Пароль"></label>
-            <span className={`error auth__span-error ${(password.empty && password.isDirty) ? `visible` : ``}`}>Введите пароль</span>
+            <span className={`error auth__span-error ${(password.empty && password.isDirty) ? `visible` : ``}`}>{PASSWORD_NOT_FOUND}</span>
           </div>
 
           <span className="error auth__error">{props.message}</span>
